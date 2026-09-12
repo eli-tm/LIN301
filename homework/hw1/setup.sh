91fe414c1b11724842f929bfab317da456aba80c
Eli Trainor <emtr232@uky.edu>
2026-09-12 17:10:21 -0400

    working on hw1

---
+++ homework/hw1/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir backup_check
+curl -o holmes.txt https://www.gutenberg.org/cache/epub/1661/pg1661.txt
+mv holmes.txt backup_check/holmes.txt
+echo "Downloaded and moved holmes.txt to backup_check directory."
+EOF
