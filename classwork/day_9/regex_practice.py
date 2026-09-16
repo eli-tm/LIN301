5305edc60957ad7fa400766edf8aaa107b9c616d
Eli Trainor <emtr232@uky.edu>
2026-09-16 12:11:18 -0400

    python script attempt 2

--- classwork/day_9/regex_practice.py
+++ classwork/day_9/regex_practice.py
@@ -1,4 +1,5 @@
-import re                                             # loads Python's regex toolkit
+import re                                                     
 
-with open("../../data/gutenberg/alice.txt", encoding="utf-8") as f:  # opens alice.txt for reading
-    text = f.read()                                   # reads the whole file into one string, called `text`
+with open("../../data/gutenberg/alice.txt", encoding="utf-8") as f:  
+    text = f.read()                                      
+matches = re.findall(r"c.t", text)                              # reads the whole file into one string, called `text`
