13888a12b191657860059a50964e8238c15f13ec
Eli Trainor <emtr232@uky.edu>
2026-09-18 18:56:28 -0400

    day 11 complete

---
+++ classwork/day_10/snacks.py
@@ -0,0 +1,6 @@
+SNACK_BUDGET = 15
+price = 2.5
+quantity = 4
+total_spent = quantity*price
+money_left = SNACK_BUDGET-total_spent
+print(total_spent, money_left)
--- classwork/day_9/regex_practice.py
+++ classwork/day_9/regex_practice.py
@@ -2,4 +2,9 @@ import re
 
 with open("../../data/gutenberg/alice.txt", encoding="utf-8") as f:  
     text = f.read()                                      
-matches = re.findall(r"c.t", text)                              # reads the whole file into one string, called `text`
+# matches = re.findall(r"c.t", text)       
+# print(matches)
+
+matches_aou = re.findall(r"c[aou]t", text)   # finds "cat", "cot", or "cut"
+print(len(matches_aou))                      # counts how many were found
+print(*matches_aou, sep="\n")                # prints each match on a separate line                      # reads the whole file into one string, called `text`
